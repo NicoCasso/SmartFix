730a1338bc1a878f70e61a0d44a6bfd020f25857
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-07-09 09:29:53 +0200

    change data directory

--- SmartFixWeb/SmartFixWeb/settings.py
+++ SmartFixWeb/SmartFixWeb/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DATA_DIR = BASE_DIR.parent / 'DATA'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -83,8 +83,6 @@ WSGI_APPLICATION = 'SmartFixWeb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATA_DIR = BASE_DIR / 'DATA'
 
 DATABASES = {
     'default': {
